cc69723139584263679eda383f61ff14a189e662
iliaadam <iliaadam@csd.auth.gr>
2022-06-07 19:49:26 +0300

    backpressed#2

--- app/src/main/java/com/example/beerlover/MainActivity.java
+++ app/src/main/java/com/example/beerlover/MainActivity.java
@@ -119,13 +119,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
 
-        long pressedTime = 0;
-        if (pressedTime + 2000 > System.currentTimeMillis()) {
-            super.onBackPressed();
             finish();
-        } else {
-            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
-        }
-        pressedTime = System.currentTimeMillis();
+
     }
 }
